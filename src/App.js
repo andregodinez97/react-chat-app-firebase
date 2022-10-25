42fbca8ec6926d053b38acc5fb0c22769de658f3
Andre Godinez <andregodinez97@gmail.com>
2022-10-25 16:11:50 +0100

    initial app with login and register pages

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import './style.scss'
+
+function App() {
+  return (
+    <Login></Login>
+  );
+}
+
+export default App;
